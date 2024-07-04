d32d7e097bbc8d13b49904bc4b508a67645f665d
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-07-04 12:32:10 +0200

    Initial Commit

---
+++ SwiftUI Apple Frameworks App/SwiftUI_Apple_Frameworks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_Frameworks_AppApp.swift
+//  SwiftUI Apple Frameworks App
+//
+//  Created by Macbook Air 2017 on 4. 7. 2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_Frameworks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
