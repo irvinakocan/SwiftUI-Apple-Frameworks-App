0e80f78935b6654fa0d067debee8779911ae0b2e
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-07-04 17:59:35 +0200

    UI implemented
    Implemented UI with scrollview and LazyVGrid using mock data.

---
+++ SwiftUI Apple Frameworks App/FrameworkGridView.swift
@@ -0,0 +1,66 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI Apple Frameworks App
+//
+//  Created by Macbook Air 2017 on 4. 7. 2024..
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks,
+                            // We can remove id part because SwiftUI knows it through the data type
+                            content: {
+                        framework in
+                        FrameworkTitleView(
+                            framework: framework)
+                    })
+                }
+                .padding(.top, 20)
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 14")
+    }
+}
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(
+                    width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                // You have the ability to shrink,
+                .scaledToFit()
+                // but you only can shrink this much
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
--- SwiftUI Apple Frameworks App/SwiftUI_Apple_Frameworks_AppApp.swift
+++ SwiftUI Apple Frameworks App/SwiftUI_Apple_Frameworks_AppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Apple_Frameworks_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
