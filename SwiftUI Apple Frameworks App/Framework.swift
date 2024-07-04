d97f42c567b7e27892790aeec4f4c47c48cf8414
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-07-04 12:39:46 +0200

    Initial commit

---
+++ SwiftUI Apple Frameworks App/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  SwiftUI Apple Frameworks App
+//
+//  Created by Macbook Air 2017 on 4. 7. 2024..
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
